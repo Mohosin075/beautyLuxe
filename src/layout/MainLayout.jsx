d42ebfd6e85b15876a92e3e6c9b0f79f38468671
Mohosin <mohosinali075@gmail.com>
2024-12-21 22:40:21 +0600

    sign up and sign in page design

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,6 +3,7 @@ import { IoHome } from "react-icons/io5";
 import { MdContactPhone } from "react-icons/md";
 import { FaAccusoft } from "react-icons/fa";
 import { AiOutlineProduct } from "react-icons/ai";
+import { IoMdLogIn } from "react-icons/io";
 import { NavLink, Outlet } from "react-router";
 import Logo from "../components/Logo";
 
@@ -35,8 +36,10 @@ function MainLayout() {
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         <div>
-          <header className="bg-purple-400 flex lg:hidden justify-between items-center py-2 px-4">
-            <Logo />
+          <header className="bg-purple-400 sticky top-0 flex lg:bg-purple-200  justify-between items-center py-2 lg:py-0 px-4">
+            <span className="lg:hidden">
+              <Logo />
+            </span>
             <label
               htmlFor="my-drawer-2"
               className="drawer-button lg:hidden text-xl text-white cursor-pointer"
@@ -53,13 +56,35 @@ function MainLayout() {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 ">
-          <div className="mb-5">
-            <Logo />
+        <ul className="menu bg-purple-200 text-base-content min-h-full w-60 p-4 ">
+          <div className="flex flex-col space-y-8 mb-4">
+            <div className="flex justify-center lg:justify-start">
+              <Logo />
+            </div>
+
+            <div className="flex justify-center gap-5">
+              <NavLink to="/sign-up" className={"my-btn bg-purple-300"}>
+                <span>
+                  <IoMdLogIn />
+                </span>
+                Sign Up
+              </NavLink>
+              <NavLink
+                to="/sign-in"
+                className={
+                  "my-btn bg-primary-dark text-purple-100 hover:bg-purple-300 hover:text-black "
+                }
+              >
+                <span>
+                  <IoMdLogIn />
+                </span>
+                Sign In
+              </NavLink>
+            </div>
           </div>
           {/* Sidebar content here */}
           {NavLinkList.map((list, i) => (
-            <li key={i}>
+            <li key={i} className="mb-1">
               <NavLink to={list.to}>
                 <span>{list.icon}</span>
                 {list.label}
